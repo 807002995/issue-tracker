b0f466a594e3a0ee68f937a992b9e94073b7af07
fanrunzhang <fanrunzhang@mzywx.com>
2025-06-18 16:40:29 +0800

    pathname

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,9 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
+import classnames from 'classnames'
 
 export const NavBar = () => {
     const linkList = [
@@ -13,13 +16,23 @@ export const NavBar = () => {
             href: '/issues'
         }
     ]
+
+    const pathName = usePathname()
+    console.log(pathName)
+    
   return (
     <nav className='flex space-x-5 border-b-1 items-center h-14 mb-5 px-5'>
           <Link href="/"><AiFillBug /></Link>  
           <ul >
               {linkList.map(link =>
               (<Link
-                  className='px-5 py-1 rounded hover:bg-gray-200'
+                  className={classnames({
+                      'bg-gray-200': pathName === link.href,
+                      'px-5': true,
+                      'py-1': true,
+                      'rounded': true,
+                      'hover:bg-gray-200': true 
+                  })}
                   key={link.href}
                   href={link.href}>{link.title}</Link>))}
           </ul>  
